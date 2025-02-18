04d4472aa589f74d7e2db0924aa2d11039977e72
hunghoang <hth321@gmail.com>
2025-02-18 10:57:15 +0700

    udpate

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -3,10 +3,11 @@ import path from "path";
 
 export const getDatabasePath = () => {
   // In development
-  if (process.env.NODE_ENV === "development") {
-    return "file:local.db";
+  if (app.isPackaged) {
+    // In production, store in user data directory
+    return `file:${path.join(app.getPath("userData"), "local.db")}`;
   }
-  
-  // In production, store in user data directory
-  return `file:${path.join(app.getPath("userData"), "local.db")}`;
+
+  // In development
+  return "file:local.db";
 };
