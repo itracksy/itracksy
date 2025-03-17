ce55c491dce86602785ef65c209e87b628fd70d2
hunghoang <hth321@gmail.com>
2025-03-17 15:33:05 +0700

    fix

--- src/api/db/index.ts
+++ src/api/db/index.ts
@@ -6,7 +6,6 @@ import * as schema from "./schema";
 const client = createClient({
   url: getDatabasePath(),
   authToken: "",
-  syncUrl: "https://api.libsql.com/v1/sync",
 });
 const db = drizzle(client, { schema });
 
