dba4bf0b3be1b7013b915d6cec0945bc14977770
hunghoang <hth321@gmail.com>
2025-05-13 14:50:46 +0700

    refactor: remove DragWindowRegion component from base layout

--- src/layouts/BaseLayout.tsx
+++ src/layouts/BaseLayout.tsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Toaster } from "@/components/ui/toaster";
 
-import DragWindowRegion from "@/components/DragWindowRegion";
-
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 
 export default function BaseLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex h-screen flex-col">
-      <DragWindowRegion />
       <div className="flex flex-1 overflow-hidden">
         <SidebarProvider>
           <AppSidebar />
